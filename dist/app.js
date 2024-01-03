64baaa94ab39c8efa8c374f10c051ccc2738bead
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-01-03 18:51:30 -0500

    updated files

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,14 @@
-console.log("working")
+// Defining Our Weight Class
+
+const Person = class Person {
+  constructor(name, weight, height) {
+    this.name = name; //this sets our property
+    this.weight = weight;
+    this.height = height;
+    this.weightLog = [];
+  }
+};
+
+
+
+
