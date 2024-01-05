099fac29ffaf966165eeb4ca4cd3f8e3ebe1b99d
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-01-05 17:34:07 -0500

    testing

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+
+
+const one = document.createElement("h1")
+one.innerText = "HELLO"
+one.className = "bg-red-500 text-3xl"
+const body = document.querySelector("body");
+body.appendChild(one)
+
+
+
