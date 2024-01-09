64e79a1dde749067572339df2fb276d58fe33f16
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-01-08 19:12:24 -0500

    updated ui

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,36 @@
+// Getting Weather Api
+const weatherApi = async () => {
+  // Api Key - do not push
 
 
-const one = document.createElement("h1")
-one.innerText = "HELLO"
-one.className = "bg-red-500 text-3xl"
-const body = document.querySelector("body");
-body.appendChild(one)
+  // Api
+  const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=40.79857&lon=-73.96659&appid=${apiKey}&units=imperial`;
+  const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=40.79857&lon=-73.96659&appid=${apiKey}&units=imperial`;
 
+  // Fetching Api Data
+  try {
+    // Current Weather
+    const responseWeather = await fetch(urlWeather);
+    const dataWeather = await responseWeather.json();
+    console.log(dataWeather);
 
+    // Forecast Weather
+    const responseForecast = await fetch(urlForecast);
+    const dataForecast = await responseForecast.json();
+    console.log(dataForecast);
 
+    weatherDomInfo(dataWeather);
+  } catch (error) {
+    console.log("Error Please Fix It: ", error);
+  }
+};
+weatherApi();
+
+const weatherIcons = () => {
+
+  return { sunny };
+};
+// Dom information
+const weatherDomInfo = (dataWeather) => {
+ const weatherSizing = ['bg-cover', 'bg-center', 'h-screen']
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}",   "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'sunny-bg': "url('/images/sunny.png')",
+        'rainy-bg': "url('/images/rainy.png')",
+        'cloudy-bg': "url('/images/cloudy.png')",
+        'windy-bg': "url('/images/windy.png')",
+        'stormy-bg': "url('/images/stormy.png')",
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
