e78e009a18c048d4b82e411a9b4fe15c33b540ff
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-01-12 18:57:29 -0500

    still working on search functionality

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,15 @@
+// Setting initial current value
+const userInput = document.querySelector("#default-search");
+const submitButton = document.querySelector("#submit-button");
+// Api Key - do not push
+
+let userInputValue = (userInput.value = "new york");
+// Api
+let urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${userInputValue}&appid=${apiKey}&units=imperial`;
+let urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${userInputValue}&appid=${apiKey}&units=imperial`;
 // Getting Weather Api
 const weatherApi = async () => {
-  // Setting initial current value
-  const userInput = document.querySelector("#default-search");
-  let userInputValue = (userInput.value = "saudi arabia");
-  const submitButton = document.querySelector("#submit-button");
-  let userInputValueEncoded = encodeURIComponent(userInputValue);
-
   const fetchingWeatherData = async () => {
-    // Api Key - do not push
-  
-    // Api
-    let urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${userInputValueEncoded}&appid=${apiKey}&units=imperial`;
-    let urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${userInputValueEncoded}&appid=${apiKey}&units=imperial`;
-
     // Fetching Api Data
     try {
       // Current Weather
@@ -35,19 +32,22 @@ const weatherApi = async () => {
     }
   };
 
-  fetchingWeatherData();
-
-  // submit button not working tackling this tomorrow
-  submitButton.addEventListener("click", async () => {
-    const reset = await weatherDisplayInfo(dataWeather);
-    console.log(reset.currentWeatherContainer);
-    reset.currentWeatherContainer.innerText = " ";
-    userInputValue = userInput.value;
-    userInputValueEncoded = encodeURIComponent(userInputValue);
-
-    await fetchingWeatherData();
-  });
+  await fetchingWeatherData();
 };
+
+const currentWeather = document.querySelector("#current-weather");
+const currentWeatherContainer = document.querySelector(
+  "#current-weather-container"
+);
+let currentFahrenheitDegree = document.querySelector(
+  "#current-fahrenheit-degree"
+);
+let currentWeatherLocation = document.querySelector(
+  "#current-weather-location"
+);
+let currentFahrenheit = document.querySelector("#current-fahrenheit");
+let currentDescription = document.querySelector("#current-description");
+let timeDisplay = document.querySelector("#time-display");
 
 // Dom display information
 const weatherDisplayInfo = async (data) => {
@@ -75,79 +75,38 @@ const weatherDisplayInfo = async (data) => {
 
     return { sunnyIcon, cloudyIcon, rainIcon, rainStormIcon, snowIcon };
   };
-
-  // Receiving the icons
-  const icons = weatherIcons();
-
-  // Current weather container
-  const currentWeatherContainer = document.querySelector(
-    ".Current-Weather-Container"
-  );
-  // Current weather display
-  const currentWeather = document.querySelector(".current-Weather");
-  const currentWeatherLocation = document.createElement("h3");
-  const currentFahrenheitDegree = document.createElement("h1");
-  const currentFahrenheit = document.createElement("p");
-  const currentDescription = document.createElement("p");
-  const timeDisplay = document.createElement("p");
-  // Displays The date
-  let currentDate = new Date(
-    data.dt * 1000 + data.timezone * 1000
-  ).toDateString();
-  // Tailwind css styles
-  currentFahrenheitDegree.classList.add(
-    "text-7xl",
-    "text-white",
-    "text-center",
-    "font-mono"
-  );
-  currentFahrenheit.classList.add("text-white", "items-start", "font-mono");
-  icons.cloudyIcon.classList.add("text-white", "text-5xl", "m-4");
-  currentWeather.classList.add(
-    "flex",
-    "flex-row-reverse",
-    "items-center",
-    "justify-center"
-  );
-  currentWeatherLocation.classList.add(
-    "text-gray-400",
-    "text-xl",
-    "flex",
-    "items-center",
-    "justify-center",
-    "font-mono",
-    "mt-2"
-  );
-  timeDisplay.classList.add(
-    "text-gray-400",
-    "font-mono",
-    "text-center",
-    "mb-2",
-    "text-lg"
-  );
-  currentDescription.classList.add(
-    "text-gray-400",
-    "text-center",
-    "font-mono",
-    "mt-2"
-  );
   // Adding current weather data
-  currentFahrenheitDegree.innerText = `${Math.round(data.main.temp)}`;
-  currentWeatherLocation.innerText = `${data.name}`;
-  currentFahrenheit.innerText = "°F";
-  currentDescription.innerText = `${data.weather[0].main} | ${data.weather[0].description}`;
-  timeDisplay.innerText = currentDate;
-  // Appending current weather data
-  currentWeatherContainer.appendChild(timeDisplay);
-  currentWeather.appendChild(currentFahrenheit);
-  currentWeather.appendChild(currentFahrenheitDegree);
-  currentWeather.appendChild(icons.cloudyIcon);
-  currentWeatherContainer.appendChild(currentWeather);
-  currentWeatherContainer.appendChild(currentWeatherLocation);
-  currentWeatherContainer.appendChild(currentDescription);
-
-  return { currentWeatherContainer };
+  const getItems = (data) => {
+    let currentDate = new Date(
+      data.dt * 1000 + data.timezone * 1000
+    ).toDateString();
+    currentFahrenheitDegree.innerText = `${Math.round(data.main.temp)}`;
+    currentWeatherLocation.innerText = `${data.name}`;
+    currentFahrenheit.innerText = "°F";
+    currentDescription.innerText = `${data.weather[0].main} | ${data.weather[0].description}`;
+    timeDisplay.innerText = currentDate;
+  };
+  return getItems(data);
 };
+
+const handleUserInput = async () => {
+  userInputValue = userInput.value = "saudi arabia";
+  urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${userInputValue}&appid=${apiKey}&units=imperial`;
+  try {
+    const responseWeather = await fetch(urlWeather);
+    if (!responseWeather.ok) {
+      throw new Error("Failed to fetch current weather data");
+    }
+    const dataWeather = await responseWeather.json();
+    console.log(dataWeather);
+    // Update the weather display with the new data
+    weatherDisplayInfo(dataWeather).getItems;
+  } catch (error) {
+    console.log("Error Please Fix It: ", error);
+  }
+};
+
+submitButton.addEventListener("click", handleUserInput);
 
 // Call the weatherApi function
 weatherApi();
