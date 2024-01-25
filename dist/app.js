6abebb3348506f2ce10f58bf323e173ddace341f
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-01-25 15:57:25 -0500

    updating forecast ui and functionality

--- dist/app.js
+++ dist/app.js
@@ -4,14 +4,13 @@ const submitButton = document.querySelector("#submit-button");
 // Api Key - do not push
 
 let userInputValue = (userInput.value = "new york").toUpperCase();
-// Api
+// Api url's
 let urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${userInputValue}&appid=${apiKey}&units=imperial`;
 let urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${userInputValue}&appid=${apiKey}&units=imperial`;
 // Getting Weather Api
 const weatherApi = async () => {
-  // Fetching Api Data
   try {
-    // Current Weather
+    // Fetching Api Data
     const responseWeather = await fetch(urlWeather);
     if (!responseWeather.ok) {
       alert("try again or enter a different location");
@@ -19,7 +18,7 @@ const weatherApi = async () => {
     }
     const dataWeather = await responseWeather.json();
     console.log(dataWeather);
-    weatherDisplayInfo(dataWeather);
+    CurrentWeatherDisplayInfo(dataWeather);
     weatherIcons(dataWeather.weather[0].main);
     // Forecast Weather
     const responseForecast = await fetch(urlForecast);
@@ -28,14 +27,14 @@ const weatherApi = async () => {
     }
     const dataForecast = await responseForecast.json();
     console.log(dataForecast);
-    console.log(dataForecast.list[2].main);
+    forecastDisplayInfo(dataForecast)
   } catch (error) {
-    console.log("Error Please Fix It: ", error);
+    console.log("Error Please Fix It:", error);
   }
 };
 
 // Dom display information
-const weatherDisplayInfo = (data) => {
+const CurrentWeatherDisplayInfo = (data) => {
   let currentFahrenheitDegree = document.querySelector(
     "#current-fahrenheit-degree"
   );
@@ -57,7 +56,40 @@ const weatherDisplayInfo = (data) => {
 };
 
 // Dom Forecast display information
-const forecastDisplayInfo = (data) => {};
+const forecastDisplayInfo = (data) => {
+  // forecast date tags
+  const secondDay = document.querySelector("#second-day");
+  const thirdDay = document.querySelector("#third-day");
+  const fourthDay = document.querySelector("#fourth-day");
+  const fifthDay = document.querySelector("#fifth-day");
+  const sixthDay = document.querySelector("#sixth-day");
+
+  // forecast fahrenheit tags
+  const secondDayFahrenheit = document.querySelector("#second-day-fahrenheit");
+  const thirdDayFahrenheit = document.querySelector("#third-day-fahrenheit");
+  const fourthDayFahrenheit = document.querySelector("#fourth-day-fahrenheit");
+  const fifthDayFahrenheit = document.querySelector("#fifth-day-fahrenheit");
+  const sixthDayFahrenheit = document.querySelector("#sixth-day-fahrenheit");
+
+  // forecast description tags
+
+  // Adding forecast data
+  // second
+  secondDay.innerText = data.list[1].dt_txt.split(" ")[0];
+  secondDayFahrenheit.innerText = `${data.list[1].main.temp} °F`
+  // third
+  thirdDay.innerText = data.list[9].dt_txt.split(" ")[0]
+  thirdDayFahrenheit.innerText = `${data.list[9].main.temp} °F`
+  // fourth
+  fourthDay.innerText = data.list[17].dt_txt.split(" ")[0]
+  fourthDayFahrenheit.innerText = `${data.list[17].main.temp} °F`
+  // fifth
+  fifthDay.innerText = data.list[25].dt_txt.split(" ")[0]
+  fifthDayFahrenheit.innerText = `${data.list[25].main.temp} °F`
+  // sixth
+  sixthDay.innerText = data.list[33].dt_txt.split(" ")[0]
+  sixthDayFahrenheit.innerText = `${data.list[33].main.temp} °F`
+};
 // Icons for the dom
 const weatherIcons = (data) => {
   // current icon
