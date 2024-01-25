b0a1bbb10b07e278d53d0fdc0a8f12eb8f3b6195
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-01-25 12:23:08 -0500

    updated icon ui

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,7 @@ const userInput = document.querySelector("#default-search");
 const submitButton = document.querySelector("#submit-button");
 // Api Key - do not push
 
-let userInputValue = (userInput.value = "new york");
+let userInputValue = (userInput.value = "new york").toUpperCase();
 // Api
 let urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${userInputValue}&appid=${apiKey}&units=imperial`;
 let urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${userInputValue}&appid=${apiKey}&units=imperial`;
@@ -14,11 +14,13 @@ const weatherApi = async () => {
     // Current Weather
     const responseWeather = await fetch(urlWeather);
     if (!responseWeather.ok) {
+      alert("try again or enter a different location")
       throw new Error("Failed to fetch current weather data");
     }
     const dataWeather = await responseWeather.json();
     console.log(dataWeather);
     weatherDisplayInfo(dataWeather);
+    weatherIcons(dataWeather);
     // Forecast Weather
     const responseForecast = await fetch(urlForecast);
     if (!responseForecast.ok) {
@@ -47,33 +49,6 @@ let timeDisplay = document.querySelector("#time-display");
 
 // Dom display information
 const weatherDisplayInfo = (data) => {
-  // Icons for the dom
-  const weatherIcons = () => {
-    // sunny icon
-    const sunnyIcon = document.createElement("i");
-    sunnyIcon.classList.add("bi", "bi-sun");
-
-    // cloudy icon
-    const cloudyIcon = document.createElement("i");
-    cloudyIcon.classList.add("bi", "bi-cloud");
-
-    // cloudy rain icon
-    const rainIcon = document.createElement("i");
-    rainIcon.classList.add("bi", "bi-cloud-rain");
-
-    // rain storm icon
-    const rainStormIcon = document.createElement("i");
-    rainStormIcon.classList.add("bi", "bi-cloud-lightning-rain");
-
-    // cloudy snow
-    const snowIcon = document.createElement("i");
-    snowIcon.classList.add("bi", "bi-cloud-snow");
-
-    // array icons
-    arrayIcons = [sunnyIcon, cloudyIcon, rainIcon, rainStormIcon, snowIcon];
-
-    return { sunnyIcon, cloudyIcon, rainIcon, rainStormIcon, snowIcon };
-  };
   // Adding current weather data
   let currentDate = new Date(
     data.dt * 1000 + data.timezone * 1000
@@ -85,8 +60,60 @@ const weatherDisplayInfo = (data) => {
   timeDisplay.innerText = currentDate;
 };
 
+// Icons for the dom
+const weatherIcons = (data) => {
+  // current icon
+  const currentIcon = document.querySelector("#current-icon");
+
+  // this switch statement handles and updates the icons ui
+  switch (data.weather[0].main) {
+    case "Mist":
+      currentIcon.className = "bi bi-cloud-fog text-white text-5xl m-4";
+      break;
+      case "Fog":
+      currentIcon.className = "bi bi-cloud-fog text-white text-5xl m-4";
+      break;
+    case "Snow":
+      currentIcon.className = "bi bi-cloud-snow text-white text-5xl m-4";
+    case "Rain":
+      if (data.weather[0].description === "light rain") {
+        console.log("working");
+        currentIcon.className = "bi bi-cloud-drizzle text-white text-5xl m-4";
+      } else if (
+        data.weather[0].description === "heavy intensity rain" ||
+        "very heavy rain" ||
+        "extreme rain"
+      ) {
+        currentIcon.className =
+          "bi bi-cloud-rain-heavy text-white text-5xl m-4";
+      } else {
+        currentIcon.className = "bi bi-cloud-rain text-white text-5xl m-4";
+      }
+      break;
+    case "Drizzle":
+      currentIcon.className = "bi bi-cloud-drizzle text-white text-5xl m-4";
+      break;
+    case "Thunderstorm":
+      currentIcon.className =
+        "bi bi-cloud-lightning-rain text-white text-5xl m-4";
+      break;
+    case "Clouds":
+      currentIcon.className = "bi bi-cloud text-white text-5xl m-4";
+      break;
+    case "Clear":
+      currentIcon.className = "bi bi-sun text-white text-5xl m-4";
+      break;
+    case "Tornado":
+      currentIcon.className = "bi bi-hurricane text-white text-5xl m-4";
+      break;
+    default:
+      currentIcon.className = "bi bi-cloud text-white text-5xl m-4";
+      console.log("could not find a icon sorry ;(")
+  }
+};
+
 const handleUserInput = () => {
-  userInputValue = userInput.value;
+  userInputValue = userInput.value.toUpperCase();
   urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${userInputValue}&appid=${apiKey}&units=imperial`;
   weatherApi();
 };
